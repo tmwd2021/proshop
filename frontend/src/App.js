3424e006a059389c79045596497c0632cf622bbb
Anthony Donovan <itsme@tony-iMac.local>
2021-07-27 21:45:31 -0500

    have a basic express server up and running, next I will work on the react side with pulling in the api for the data to be pulled from the server.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,23 @@
 import { Container } from "react-bootstrap";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import Header from "./components/Header.js";
 import Footer from "./components/Footer.js";
 import HomeScreen from "./screens/HomeScreen.js";
+import ProductScreen from "./screens/ProductScreen.js";
 
 const App = () => {
   return (
-    <>
+    <Router>
       <Header />
+
       <main className="py-3">
         <Container>
-          <HomeScreen />
+          <Route exact path="/" component={HomeScreen} />
+          <Route path="/product/:id" component={ProductScreen} />
         </Container>
       </main>
       <Footer />
-    </>
+    </Router>
   );
 };
 
